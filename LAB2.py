63d283a7212973617b28a8e7461126ccaa23df07
keving-ing <s273302@studeti.polito.it>
2023-12-16 11:04:34 +0100

    Task 2

--- LAB2.py
+++ LAB2.py
@@ -26,7 +26,6 @@ def timeSeries_day(title,Bookings):
               elif (DFBookings["_id"][i]["hour"]==0) or ((DFBookings["_id"][i+1]["dow"]-DFBookings["_id"][i]["dow"])!=0) and ((DFBookings["_id"][i+1]["dow"] - DFBookings["_id"][i]["dow"])!=1):
                      ticks.append(i)
                      formatted_date = day_of_year_to_date( DFBookings["_id"][i+1]["dow"], 2017)
-                     print(formatted_date)
                      labels.append(formatted_date)
 
                     #  if formatted_date == '01':
@@ -47,7 +46,41 @@ def timeSeries_day(title,Bookings):
                   rotation=-30)
        plt.legend(loc='best')
        plt.grid(True,which="both")
-       plt.show()
+       #plt.show()
+
+       DFBookings.columns = ['time','rentals']
+
+       df = DFBookings.to_csv(path_or_buf = "C:/Users/kevin/Documents/PoliTo/SMART_MOBILITY/"+title+".csv", index = False)
+
+       return df
+
+def add_miss(df):
+    
+    df_new = pd.DataFrame(columns=['time', 'rental'])
+    for i in range(1, len(df), 1):
+        if (str(df.index[i] - df.index[i - 1]) != '0 days 01:00:00'):
+            s = pd.date_range(df.index[i - 1], df.index[i], freq='1H')
+            for j in range(1, len(s) - 1):
+                newvalue = df.iloc[i + j - 24].rental +10 * np.random.random_sample() / 100.0
+                print('Fitting', i, j, newvalue)
+                df_new = df_new.append({'Time': s[j], 'rental': newvalue}, ignore_index = True)
+    df_new = df_new.set_index('time')
+    df = df.append(df_new)
+    df = df.sort_index()
+
+    plt.plot(df)
+    plt.title('Filled rentals timeseries of ')
+    plt.xlabel('Date')
+    plt.ylabel('Number of rentals')
+    plt.xticks(rotation=50)
+    plt.grid(linestyle = '--', linewidth=0.8)
+
+    plt.show();
+
+    df.plot(title="Rentals in AMSTERDAM")
+
+
+
 
 
 if __name__ == "__main__":
@@ -124,4 +157,8 @@ if __name__ == "__main__":
                 
                 }])
 
-        timeSeries_day(city+"_timeSeries",timeSeries['Bookings'+city])
+        time_series_df = timeSeries_day(city+"_timeSeries",timeSeries['Bookings'+city])
+
+        df_n = pd.read_csv(r"C:/Users/kevin/Documents/PoliTo/SMART_MOBILITY/"+city+".csv", sep=",", parse_dates=[0], infer_datetime_format = True, date_parser=dateparse, index_col = 0)
+
+        add_miss(time_series_df)
