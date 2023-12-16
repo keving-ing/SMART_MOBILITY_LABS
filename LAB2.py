d4d47249df7b2a9ca9fc14737aef2372a31dc236
keving-ing <s273302@studeti.polito.it>
2023-12-16 14:29:01 +0100

    Sistemare l'ordinamento, RIGA 73

--- LAB2.py
+++ LAB2.py
@@ -7,6 +7,7 @@ import calendar
 import gmplot
 import math
 import mne
+import io
 
 def day_of_year_to_date(day_of_year, year):
     reference_date = datetime(year, 1, 1)
@@ -14,72 +15,81 @@ def day_of_year_to_date(day_of_year, year):
 
     return target_date.strftime('%d')
 
-def timeSeries_day(title,Bookings):
-       listBookings = list(Bookings)
-       DFBookings=pd.DataFrame(listBookings)
-       labels=[]
-       ticks=[]
+def timeSeries_day(title, Bookings):
+    listBookings = list(Bookings)
+    DFBookings = pd.DataFrame(listBookings)
+    labels = []
+    ticks = []
 
-       for i in range(DFBookings.shape[0]):
-              if i == (DFBookings.shape[0]-1):
-                     break
-              elif (DFBookings["_id"][i]["hour"]==0) or ((DFBookings["_id"][i+1]["dow"]-DFBookings["_id"][i]["dow"])!=0) and ((DFBookings["_id"][i+1]["dow"] - DFBookings["_id"][i]["dow"])!=1):
-                     ticks.append(i)
-                     formatted_date = day_of_year_to_date( DFBookings["_id"][i+1]["dow"], 2017)
-                     labels.append(formatted_date)
+    for i in range(DFBookings.shape[0] - 1):
+        if (DFBookings["_id"][i]["hour"] == 0) or (
+                (DFBookings["_id"][i + 1]["dow"] - DFBookings["_id"][i]["dow"]) != 0) and (
+                (DFBookings["_id"][i + 1]["dow"] - DFBookings["_id"][i]["dow"]) != 1):
+            ticks.append(i)
+            formatted_date = day_of_year_to_date(DFBookings["_id"][i + 1]["dow"], 2017)
+            labels.append(formatted_date)
 
-                    #  if formatted_date == '01':
-                    #        ticks.append(0)
-                    #        labels.append("02")
+    plt.figure()
+    plt.xlabel("Days of October")
+    plt.ylabel("N. of Bookings")
+    plt.title(title)
+    plt.plot(DFBookings["totOFbookings"], label="Bookings")
+    plt.xticks(ticks=ticks,
+               labels=labels,
+               rotation=-30)
+    plt.legend(loc='best')
+    plt.grid(True, which="both")
+    plt.show()
 
-                    #  if formatted_date == '23':
-                    #        ticks.append(0)
-                    #        labels.append("24")
+    DFBookings.columns = ['_id', 'totOFbookings']
 
-       plt.figure()
-       plt.xlabel("Days of October")
-       plt.ylabel("N. of Bookings")
-       plt.title(title)
-       plt.plot(DFBookings["totOFbookings"],label="Bookings")
-       plt.xticks(ticks=ticks,
-                  labels=labels,
-                  rotation=-30)
-       plt.legend(loc='best')
-       plt.grid(True,which="both")
-       #plt.show()
+    df_result = DFBookings.to_csv(index=False)  # Save to CSV without index
 
-       DFBookings.columns = ['time','rentals']
-
-       df = DFBookings.to_csv(path_or_buf = "C:/Users/kevin/Documents/PoliTo/SMART_MOBILITY/"+title+".csv", index = False)
-
-       return df
+    return df_result
 
 def add_miss(df):
+    dow_hour_combinations = [(dow, hour) for dow in range(275,305) for hour in range(24)]
+    new_rows = []
+
+    print(df)
+    for dow, hour in dow_hour_combinations:
+        # Check if the combination exists in the DataFrame
+        if not any(((eval(entry['_id'])['dow'] == dow) and (eval(entry['_id'])['hour'] == hour)) for _, entry in df.iterrows()):
+            # If not, create a new row with rentals value set to 0
+            new_rows.append({'_id': {'dow': dow, 'hour': hour}, 'totOFbookings': 0})
+
+    # Create a new DataFrame with the new rows
+    new_df = pd.DataFrame(new_rows)
+
+    # Concatenate the original DataFrame and the new DataFrame
+    df = pd.concat([df, new_df], ignore_index=True)
+
+    print(df)
+
+    # Sort the DataFrame by dow and hour
+    df['_id'] = df['_id'].apply(lambda x: {'dow': int(x['dow']) if isinstance(x, dict) else int(x.split(',')[0].split(':')[1].strip('}{ ')),
+                                       'hour': int(x['hour']) if isinstance(x, dict) else int(x.split(',')[1].split(':')[1].strip('}{ '))})
     
-    df_new = pd.DataFrame(columns=['time', 'rental'])
-    for i in range(1, len(df), 1):
-        if (str(df.index[i] - df.index[i - 1]) != '0 days 01:00:00'):
-            s = pd.date_range(df.index[i - 1], df.index[i], freq='1H')
-            for j in range(1, len(s) - 1):
-                newvalue = df.iloc[i + j - 24].rental +10 * np.random.random_sample() / 100.0
-                print('Fitting', i, j, newvalue)
-                df_new = df_new.append({'Time': s[j], 'rental': newvalue}, ignore_index = True)
-    df_new = df_new.set_index('time')
-    df = df.append(df_new)
-    df = df.sort_index()
+    df = df.sort_values(by=[('_id', 'dow'), ('_id', 'hour')]).reset_index(drop=True)
 
-    plt.plot(df)
-    plt.title('Filled rentals timeseries of ')
-    plt.xlabel('Date')
+
+    
+    plt.plot(df['rentals'])
+    plt.title('Filled rentals timeseries')
+    plt.xlabel('Index')
     plt.ylabel('Number of rentals')
-    plt.xticks(rotation=50)
-    plt.grid(linestyle = '--', linewidth=0.8)
+    plt.xticks(rotation=50
+               )
+    plt.grid(linestyle='--', linewidth=0.8)
+    plt.show()
 
-    plt.show();
+    df.plot(y='rentals', title="Rentals in AMSTERDAM")
 
-    df.plot(title="Rentals in AMSTERDAM")
+    return df
 
 
+def dateparse(time_insex):
+        return pd.datetime.fromtimestamp(float(time_insex)*3600)
 
 
 
@@ -97,7 +107,7 @@ if __name__ == "__main__":
     db = client['carsharing']
     Bookings = db['PermanentBookings']
 
-    cities=["Denver","Amsterdam","Milano"]
+    cities=["Amsterdam","Milano"]
 
     #---------------------------------------------------------------------------------------------
 
@@ -115,50 +125,50 @@ if __name__ == "__main__":
     for city in cities:
         timeSeries['Bookings{0}'.format(city)] = Bookings.aggregate([
         {"$match": {
-                    "city":city,
+                    "city": city,
                     "init_time": {"$gte": init_unix, "$lte": finish_unix}
                    }
                 },
-        {"$project":{
-                "duration":{
-                        "$subtract":["$final_time","$init_time"]
-                },
+        {"$project": {
+            "duration": {
+                "$subtract": ["$final_time", "$init_time"]
+            },
 
-                "or_de":{
-                        "$ne":[{
-                                "$arrayElemAt":["$origin_destination.coordinates",0]
-                        },
-                        {
-                                "$arrayElemAt":["$origin_destination.coordinates",1]
-                        }
-                        ]
+            "or_de": {
+                "$ne": [{
+                    "$arrayElemAt": ["$origin_destination.coordinates", 0]
                 },
-        
-            "dow":{"$dayOfYear":"$init_date"},
-            "hour" : {"$hour":"$init_date"}  }
-                    },
-                    {"$match": {
-                        "duration":{
-                                "$gte": 5*60, "$lte":150*60
-                        },
-                        "or_de": True 
+                    {
+                        "$arrayElemAt": ["$origin_destination.coordinates", 1]
                     }
-                    },
+                ]
+            },
 
-        {"$group":{
-            "_id": {"dow":"$dow","hour":"$hour"},
-            "totOFbookings":{"$sum":1}
-            
-                }
-                },
-        {"$sort":{
-                "_id":1
-            }
-                
-                }])
+            "dow": {"$dayOfYear": "$init_date"},
+            "hour": {"$hour": "$init_date"}
+        }},
+        {"$match": {
+            "duration": {
+                "$gte": 5 * 60, "$lte": 150 * 60
+            },
+            "or_de": True
+        }
+        },
 
-        time_series_df = timeSeries_day(city+"_timeSeries",timeSeries['Bookings'+city])
+        {"$group": {
+            "_id": {"dow": "$dow", "hour": "$hour"},
+            "totOFbookings": {"$sum": 1}
 
-        df_n = pd.read_csv(r"C:/Users/kevin/Documents/PoliTo/SMART_MOBILITY/"+city+".csv", sep=",", parse_dates=[0], infer_datetime_format = True, date_parser=dateparse, index_col = 0)
+        }
+        },
+        {"$sort": {
+            "_id": 1
 
-        add_miss(time_series_df)
+        }
+
+        }])
+
+        df = time_series_df = timeSeries_day(city + "_timeSeries", timeSeries['Bookings' + city])
+
+        # Call the function to fill in missing combinations with zero values
+        df_n = add_miss(pd.read_csv(io.StringIO(df)))
